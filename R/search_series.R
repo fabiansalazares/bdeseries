1e1ddaa97d8339b799118dcfff71d75289cff213
Fabián Salazar, Miguel <mfabian@economia.gob.es>
2023-08-16 14:34:02 +0200

    20230816 v0.11 search_series con matching AND a nivel de elemento de lista y OR a nivel de palabra

--- R/search_series.R
+++ R/search_series.R
@@ -2,8 +2,11 @@
 #'
 #' This function takes a list of search strings and returns a list of dataframes containing the results for each search string.
 #'
-#' By default, search_series() matches field "descripcion" of the Banco de España series catalog. However, the field to be matched
-#' can be modified by passing values [ | ] to argument 'field'. I.e.: search_series(search_str=c("economía internacional", "España"), field=)
+#' The list of search strings are all matched AND-wise at the list element level, and OR-wise at the word level within a list element. Example:
+#' search_str <- c("Economía internacional", "Italia") would be matched against the chosen field as follows: ("Economía internacional" OR "Italia") AND ("Italia")
+#'
+#' By default, search_series() matches each field "descripcion" of the Banco de España series catalog. However, the field to be matched
+#' can be modified by passing the name of the variable to be matched against to argument 'field', i.e.: search_series(search_str=c("economía internacional", "España"), field="unidades")
 #' @param search_str search string(s) to be matched.
 #' @keywords search series
 #' @export
@@ -17,13 +20,18 @@ search_series <- function(search_str,
                      "\\\\", tools::R_user_dir("bdeseries", which = "data"))
 
   # series_catalog <- feather::read_feather(paste0(datos_path, "\\catalogo_be.feather"))
-  series_catalog <- feather::read_feather(paste0(datos_path, "\\catalogo.feather"))
+  results <- feather::read_feather(paste0(datos_path, "\\catalogo.feather"))
 
-  lapply(search_str, function(x) {
-    series_catalog |> dplyr::filter(grepl(stringr::str_replace(x, " ", "_"),
-                                                                        descripcion,
-                                                                        ignore.case=TRUE))
-    }
-         )
+  for (search_item in search_str) {
+    results <- results |> dplyr::filter(grepl(stringr::str_replace(search_item,
+                                                                   " ",
+                                                                   "|"),
+                                              eval(as.symbol(field)),
+                                              ignore.case=TRUE))
+  }
+
+
+
+  return(results)
 
 }

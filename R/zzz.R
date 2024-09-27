a2050eeac34af834ecad45691db5e92ee303a3d5
Miguel Fabián Salazar <miguel@fabiansalazar.es>
2024-09-27 10:32:27 +0200

    2024092027

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,10 @@
 .onLoad <- function(...) {
   packageStartupMessage("bdeseries 0.50-20240927 - miguel@fabiansalazar.es")
 
-  if(!getOption("bdeseries_disable_autoupdate")) {
-    download_series_full()
+  if(!is.null(getOption("bdeseries_disable_autoupdate"))) {
+    if(!getOption("bdeseries_disable_autoupdate")) {
+      download_series_full()
+    }
   } else {
     message("Auto update has been disabled. To update the series' database run: download_series_full()")
   }
